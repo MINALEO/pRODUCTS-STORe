ad67f722b0f823f97808f4eeccdc0a194ccd2e33
MINALEO <109306595+MINALEO@users.noreply.github.com>
2021-12-06 23:00:00 +0000

    fix i18n

--- src/formatted.rs
+++ src/formatted.rs
@@ -3,4 +3,5 @@ use matrix_sdk::ruma::events::room::message::RoomMessageEventContent;
 use rspotify::{model::FullTrack, ClientError};
 
 pub fn track(track: &FullTrack) -> String {
-    format!
+    format!(
+      
