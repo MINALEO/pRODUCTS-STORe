dfc1f4aa926542cefbb0c3a6ed01babdc6dd3c9b
MINALEO <109306595+MINALEO@users.noreply.github.com>
2022-03-12 22:11:58 +0000

    swift 4

--- src/formatted.rs
+++ src/formatted.rs
@@ -27,4 +27,4 @@ pub async fn error(error: Error) -> RoomMessageEventContent {
             }
             rspotify::http::HttpError::Client(_) => todo!(),
         },
-        Ok(e) => RoomMessageEventContent::text_pla
+        Ok(e) => RoomMessageEventContent::text_plain(e.to_string
