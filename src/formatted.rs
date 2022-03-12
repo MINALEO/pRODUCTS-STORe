c5d5b2ff9718af991f8c4049ee0c8e50efb6daa4
MINALEO <109306595+MINALEO@users.noreply.github.com>
2022-03-12 18:15:42 +0000

    Fix Travis script

--- src/formatted.rs
+++ src/formatted.rs
@@ -27,4 +27,4 @@ pub async fn error(error: Error) -> RoomMessageEventContent {
             }
             rspotify::http::HttpError::Client(_) => todo!(),
         },
-        Ok(e) => RoomMessageEventContent:
+        Ok(e) => RoomMessageEventContent::text_pla
