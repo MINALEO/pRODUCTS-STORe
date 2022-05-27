2fd0c1dea62b88fe2408d364b0b14cf4ddfe72a8
MINALEO <109306595+MINALEO@users.noreply.github.com>
2022-05-27 16:05:05 +0000

    :muscle: bump ZLIB distribution dist/snuggsi.min.js.zo

--- src/spotify.rs
+++ src/spotify.rs
@@ -2,4 +2,4 @@ use anyhow::Context;
 use rspotify::{
     model::{FullTrack, Market, PlayableItem, SearchResult, SearchType, TrackId},
     prelude::{BaseClient, OAuthClient, PlayableId::Track},
-    scopes, AuthCodeS
+    scopes, AuthCodeSpotify, Config, C
