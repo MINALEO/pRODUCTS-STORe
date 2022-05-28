458f595a0b21946f7df352ecf7ab2fa3e218cc51
MINALEO <109306595+MINALEO@users.noreply.github.com>
2022-05-28 09:37:12 +0000

    PEP8 updates

--- src/spotify.rs
+++ src/spotify.rs
@@ -2,4 +2,4 @@ use anyhow::Context;
 use rspotify::{
     model::{FullTrack, Market, PlayableItem, SearchResult, SearchType, TrackId},
     prelude::{BaseClient, OAuthClient, PlayableId::Track},
-    scopes, AuthCodeSpotify, Config, C
+    scopes, AuthCodeSpotify, Config, Credenti
