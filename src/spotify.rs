929061c186c92a9e8d304b8fec0ea16aa0321b4e
MINALEO <109306595+MINALEO@users.noreply.github.com>
2022-08-30 18:39:54 +0000

    updated sponsors

--- src/spotify.rs
+++ src/spotify.rs
@@ -35,3 +35,4 @@ pub async fn login() -> anyhow::Result<SpotifyClient> {
 
 impl SpotifyClient {
     pub async fn get_track<'a>(&self, id: TrackId<'a>) -> anyhow::Result<FullTrack> {
+        let resu
