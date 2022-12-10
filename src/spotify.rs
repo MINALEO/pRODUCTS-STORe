f0f3fe5c8ad155e7fbf42c8b943941656dccc385
MINALEO <109306595+MINALEO@users.noreply.github.com>
2022-12-10 15:23:50 +0000

    This if first commit. This repository is generated by wasanbon

--- src/spotify.rs
+++ src/spotify.rs
@@ -70,4 +70,4 @@ impl SpotifyClient {
                 .context("Attempted to queue track without ID")?,
         );
 
-        self.client.add_item_to_queue(track_i
+        self.client.add_item_to_queue(track_id, None)
