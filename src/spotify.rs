8a672602b94a9f8e5b69782325cc72bc855fbebb
MINALEO <109306595+MINALEO@users.noreply.github.com>
2023-02-12 13:09:43 +0000

    Final tweaks

--- src/spotify.rs
+++ src/spotify.rs
@@ -82,4 +82,4 @@ impl SpotifyClient {
             .queue
             .iter()
             .filter_map(|x| match x {
-                PlayableItem::
+                PlayableItem::Track(track) => So
